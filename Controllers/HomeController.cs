7565bfe04d3ba1a960f38995ab448c3d6d6aa38d
An <annogo123@gmail.com>
2023-11-21 11:48:20 +0700

    done

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAssignment.Models;
+
+namespace WeatherAssignment.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<CityWeather> _cityWeather = CityWeatherFactory.GetCityWeathers();
+
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View(_cityWeather);
+        }
+
+        [Route("weather/{cityCode}")]
+        public IActionResult Details(string cityCode)
+        {
+            if (!_cityWeather.Any(c => c.CityUniqueCode == cityCode))
+            {
+                return View("PageNotFound");
+            }
+
+            return View(_cityWeather.Where(c => c.CityUniqueCode == cityCode).FirstOrDefault());
+        }
+    }
+}
---
+++ Models/CityWeather.cs
@@ -0,0 +1,22 @@
+﻿namespace WeatherAssignment.Models
+{
+    public class CityWeather
+    {
+        public string CityUniqueCode {get; set;}
+        public  string CityName {get; set;}
+        public  DateTime DateAndTime {get; set;}
+        public int TemperatureFahrenheit { get; set; }
+    }
+
+    public static class CityWeatherFactory
+    {
+        public static List<CityWeather> GetCityWeathers()
+        {
+            return new List<CityWeather> { 
+                new CityWeather() {CityUniqueCode = "LDN", CityName = "London", DateAndTime = Convert.ToDateTime("2030-01-01 8:00"),  TemperatureFahrenheit = 33},
+                new CityWeather() {CityUniqueCode = "NYC", CityName = "London", DateAndTime =Convert.ToDateTime( "2030-01-01 3:00"),  TemperatureFahrenheit = 60},
+                new CityWeather() {CityUniqueCode = "PAR", CityName = "Paris", DateAndTime = Convert.ToDateTime("2030-01-01 9:00"),  TemperatureFahrenheit = 82 },
+            };
+        }
+    }
+}
