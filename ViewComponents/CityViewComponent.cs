264fa7cf6687f0632f1a7fc30f3fac1d2cd4cace
An <annogo123@gmail.com>
2023-11-22 10:08:22 +0700

    done

---
+++ ViewComponents/CityViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WeatherAssignment.Models;
+
+namespace WeatherAssignment.ViewComponents
+{
+
+    [ViewComponent]
+    public class CityViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync(CityWeather city)
+        {
+            return View(city);
+        }
+    }
+}
